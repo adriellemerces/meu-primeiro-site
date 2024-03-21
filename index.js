e9b33875049987cfad5856f85025c7d733ca588a
Adrielle Merces <125903501+adriellemerces@users.noreply.github.com>
2024-03-21 18:25:12 -0300

    Atualizações de interações na página

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var elementosDuvida = document.querySelectorAll('.duvida')
 
-elementosDuvida.forEach(function (duvida){
-  duvida.addEventListener('click', function(){
-     duvida.classList.add('fundo-verde')
+elementosDuvida.forEach(function (duvida) {
+  duvida.addEventListener('click', function () {
+     duvida.classList.toggle('ativa')
   })
 })
