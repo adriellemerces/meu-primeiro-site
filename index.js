e01fd0036fa20342019b8c180c0c1ef666e5b48f
Adrielle Merces <125903501+adriellemerces@users.noreply.github.com>
2024-03-21 16:16:10 -0300

    Edição index.js
    Criando as interações da página com JavaScript

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-alert("Olá Mundo!")
+var elementosDuvida = document.querySelectorAll('.duvida')
+
+elementosDuvida.forEach(function (duvida){
+  duvida.addEventListener('click', function(){
+     duvida.classList.add('fundo-verde')
+  })
+})
